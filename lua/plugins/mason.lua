6e53c01c2e1b5fa32d42e7497dd025465cb27e1b
Jayden Grubb <jaydengrubb1@gmail.com>
2024-08-02 19:52:43 +1000

    Change conflicting keymappings

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -27,7 +27,7 @@ return {
 			lspconfig.lua_ls.setup({})
 			lspconfig.clangd.setup({})
 
-			vim.keymap.set("n", "<C-k>", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<C-u>", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
 		end,
