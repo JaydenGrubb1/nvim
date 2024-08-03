1e14748b73a70fe759e72aaed83b8d17c89bd28b
Jayden Grubb <jaydengrubb1@gmail.com>
2024-08-04 02:14:25 +1000

    Remove fileformat from default lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,10 +1,15 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	config = function()
-		require("lualine").setup({
-			options = {
-				theme = "dracula",
-			},
-		})
-	end,
+    "nvim-lualine/lualine.nvim",
+    config = function()
+        -- remove "fileformat" from default config
+        local default_config = require("lualine").get_config()
+        default_config.sections.lualine_x = { "encoding", "filetype" }
+
+        require("lualine").setup({
+            options = {
+                theme = "catppuccin",
+            },
+            sections = default_config.sections,
+        })
+    end,
 }
